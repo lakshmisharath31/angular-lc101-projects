ec3ece7aa25400fa9177a302a2dacc274ba7f494
Lakshmi Sharath <lakshmi@lakshmis-air.home>
2020-05-22 10:21:58 -0400

    files updated

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var require : any
 
 @Component({
   selector: 'fav-photos',
@@ -6,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
-
+  image2 = require("src/assets/img/img.png")
+  image3 = require("src/assets/img/pencil.gif")
+  image4 = require("src/assets/img/bfly.png")
   constructor() { }
 
   ngOnInit() {
